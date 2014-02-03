cff8646697e1119a3ce5b1dc1b3046fb97097dd9
Eric Streske <estreske@gmail.com>
2014-02-03 13:34:53 -0500

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Balderdash::Application.config.secret_token = 'e986d7e189eb7c6abc08e3c88cf9c9a1222726308943b1280102deb7f7efdc9fab9f6151f4ef6c2c7bacfc5c97a717d53e6c9bb3d05ef46d76e78a4e4759ccbb'
