9b479fd263feda1dfb89ccbe6f47260ef6ac5ef1
Sarah <sarah@spoonuniversity.com>
2014-02-03 18:25:06 -0500

    working tests

---
+++ spec/features/game_features_spec.rb
@@ -0,0 +1,44 @@
+require 'spec_helper'
+
+describe GamesController do
+	before do 
+		#session1 = Capybara::Session.new
+		@user = User.create(email: 'example@email.com', password: 'password123', name: 'Billy Joel')
+		login_as(@user)
+	end
+	describe "when on the homepage" do
+		before do 
+			@game = Game.create(in_session: false)
+			visit games_path
+		end
+		it "should display all the games" do
+			page.should have_content('Game Room #' + @game.id.to_s)
+		end
+		describe "when a user clicks on the create button" do
+			before do
+				click_button 'Create a Game'
+			end
+			it "should create the game and a player and redirect" do
+				current_path.should == game_path(Game.all.last)
+				page.should have_content('Billy Joel')
+			end
+		end
+		# describe "if there is another user" do 
+		# 	before do 
+		# 		@user_2 = User.create(email: 'example2@email.com', password: 'password', name: 'Sandra D')
+		# 		login_as(@user_2)
+		# 		@game = Game.create(in_session: false)
+		# 		Player.create(user: @user, game: @game)
+		# 	end
+		# 	describe "when they click on one of the games listed" do 
+		# 		before do 
+		# 			click_link(@game.id.to_s)
+		# 		end
+		# 		it 'should redirect to the game page' do 
+		# 			page.should have_content(@user.name)
+		# 		end
+		# 		it 'should '
+		# 	end
+		# end
+	end
+end
