b720a7dedf7d4f05fd75fd91da1db70ee1167a9a
bmwertman <brad.wertman@leanmeanz.com>
2014-02-04 15:02:36 -0500

    cleanup commit

--- spec/features/game_features_spec.rb
+++ spec/features/game_features_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 describe GamesController do
-	before do 
+	before do
 		@session1 = Capybara::Session.new(:rack_test, Balderdash::Application)
 		@user = User.create(email: 'example@email.com', password: 'password123', name: 'Billy Joel')
 		@session1.login_as(@user)
 	end
 	describe "when on the homepage" do
-		before do 
+		before do
 			@game = Game.create(in_session: false)
 			@session1.visit games_path
 		end
@@ -25,20 +25,20 @@ describe GamesController do
 			end
 		end
 	end
-	describe "if there is another user" do 
-		before do 
+	describe "if there is another user" do
+		before do
 			@session2 = Capybara::Session.new(:rack_test, Balderdash::Application)
 			@user_2 = User.create(email: 'example2@email.com', password: 'password', name: 'Sandra D')
 			@session2.login_as(@user_2)
 			@game = Game.create(in_session: false)
 			@current_player = Player.create(user: @user, game: @game)
 		end
-		describe "when they click on one of the games listed" do 
-			before do 
+		describe "when they click on one of the games listed" do
+			before do
 				@session2.visit games_path
 				@session2.click_link(@game.id.to_s)
 			end
-			it 'should redirect to the game page' do 
+			it 'should redirect to the game page' do
 				@session2.body.should have_content(@user.name)
 			end
 			describe 'when clicking join game' do
@@ -51,14 +51,14 @@ describe GamesController do
 			end
 		end
 		describe 'when the creator wants to start the game' do
-			before do 
-				binding.pry
+			before do
 				@session1.click_button('start_game')
 			end
-			it "should start the game" do 
+			it "should start the game" do
 				@game.rounds.last.word.should != nil
 			end
 		end
+
 	end
 end
 
