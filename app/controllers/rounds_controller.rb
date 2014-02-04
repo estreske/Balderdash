e642a9f9f7011ec92d785585985aceeafc572b64
Eric Streske <estreske@gmail.com>
2014-02-04 15:40:42 -0500

    changed round logic and added a complete game page

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,7 +1,10 @@
 class RoundsController < ApplicationController
 	def create 
 		game = Game.find(params[:game_id])
-		Round.begin(game)
-		redirect_to game_path(game)
+    if game.rounds.count < 7
+		  Round.begin(game)
+		  redirect_to game_path(game)
+    elsif game.rounds.count = 7
+      redirect_to game_complete_path(game)
 	end
 end
