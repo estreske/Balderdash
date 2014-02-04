fe9d2fbe52f8200a6d2d0cf092f9e670eaf98bbc
johnsamuelgordon <johnsamuelgordon@gmail.com>
2014-02-04 14:31:35 -0500

    sorted devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  def after_sign_in_path_for(resource)
+    "/games" # <- Path you want to redirect the user to.
+  end
+
+
 end
