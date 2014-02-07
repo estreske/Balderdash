5d6089b811842f4e0e12a9d8299ce1612d61db3e
Eric Streske <estreske@gmail.com>
2014-02-06 20:15:44 -0500

    created dynamicSort function to sort player scores

--- app/assets/javascripts/new.js
+++ app/assets/javascripts/new.js
@@ -87,10 +87,23 @@ PlayerView.prototype = {
   render: function(){
     var self = this;
     $(this.tbody).empty();
-    $(this.players).each(function(index, player) {
+    var sorted = this.players.sort(this.dynamicSort("score")).reverse()
+    $(sorted).each(function(index, player) {
       $(self.tbody).append(player.render());
     });
-  }
+  },
+
+  dynamicSort: function(property) {
+    var sortOrder = 1;
+    if(property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+    }
+    return function (a,b) {
+        var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+        return result * sortOrder;
+    }
+  } 
 }
 
 function Player(game_id, score, name, player_id){
